a6ad6906c1c78e8274e9ab2bf6180c2d59e68c4f
guswiler <guswiler@arizona.edu>
2024-04-12 13:00:25 -0700

    Finalized data import script and saved clean data .csv

--- Guswiler_FinalProject/scripts/tor_data_import.R
+++ Guswiler_FinalProject/scripts/tor_data_import.R
@@ -3,10 +3,10 @@ library(tidyverse)
 #### Move all these files over to final project folders, file paths will change, better to do now before you forget
 
 # Load data files into environment
-animal_info <- read_csv("data_raw/animal_info.csv")
-sample_1 <- read_csv("data_raw/sample_1_obj_4.csv")
-sample_2 <- read_csv("data_raw/sample_2_obj_19.csv")
-test <- read_csv("data_raw/test.csv")
+animal_info <- read_csv("Guswiler_FinalProject/data_raw/animal_info.csv")
+sample_1 <- read_csv("Guswiler_FinalProject/data_raw/sample_1_obj_4.csv")
+sample_2 <- read_csv("Guswiler_FinalProject/data_raw/sample_2_obj_19.csv")
+test <- read_csv("Guswiler_FinalProject/data_raw/test.csv")
 
 # Join data frames into one
 full_data <- animal_info %>% 
@@ -70,9 +70,9 @@ check_test_phase_expl <- function(test_total_expl){  # test: total time rat spen
 }
 ######
 
-# Apply functions to check for sufficient exploration
+# Apply functions to check for sufficient exploration and merge into a single column
 full_data <- full_data %>%
-  mutate(.before = barnes_id,                       #### will choose a better location for this
+  mutate(.after = disc_rat_total,                       #### will choose a better location for this
          # check sample phase 1 exploration
          sample_1_suff_expl = check_sample_phase_expl(sample_1_east_obj_4_s,
                                                       sample_1_west_obj_4_s),
@@ -81,21 +81,16 @@ full_data <- full_data %>%
                                                       sample_2_west_obj_19_s),
          # check test exploration
          test_suff_expl = check_test_phase_expl(test_total_expl_s)) %>% 
-  # merge all T/F columns into one, retaining F in any instance.
-  #### There is probably a better way to do this that will merge cols and remove the other ones at the same time
+  # merge all T/F columns into one, marking F unless all are T
   mutate(.before = sample_1_suff_expl,
          sufficient_expl = case_when(sample_1_suff_expl == TRUE &
                                        sample_2_suff_expl == TRUE &
                                        test_suff_expl == TRUE
                                      ~ TRUE,
                                      .default = FALSE)) %>% 
-  # remove individual T/F columns, see note above
+  # remove individual T/F columns used to create merged column
   select(!c(sample_1_suff_expl, sample_2_suff_expl, test_suff_expl))
 
 
 # Save data frame in data_clean/
-write_csv(full_data, "data_clean/tor_full_data.csv")
-
-
-# TO DO:
-## determine a better way to merge T/F columns
+write_csv(full_data, "Guswiler_FinalProject/data_clean/tor_full_data.csv")
